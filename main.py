8faf28244a5c4efbe2e2ad7930479b581d0660dd
Nurbol <nurbolkachkynbekov47@gmail.com>
2025-11-06 22:11:54 +0600

    add index, genres html files

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+from flask import Flask, render_template
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/profile')
+def profile():
+    return 'profile'
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/about')
+def about():
+    return 'about'
+
+
+@app.route('/genres')
+def genres():
+    return render_template('genres.html')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
