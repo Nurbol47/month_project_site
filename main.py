46c75d0d2e645e69e59a0b2186b79447e4f72d41
Nurbol <nurbolkachkynbekov47@gmail.com>
2025-11-07 21:15:39 +0600

    add create, genres, index pages

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop_book.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"Книга: {self.title}"
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    books = Book.query.all()
+    return render_template('index.html', data=books)
 
 
 @app.route('/profile')
@@ -13,14 +28,30 @@ def profile():
     return 'profile'
 
 
-@app.route('/about')
-def about():
-    return 'about'
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        author = request.form['author']
+        price = request.form['price']
+        text = request.form['text']
+
+        book = Book(title=title, author=author, price=price, text=text)
+
+        try:
+            db.session.add(book)
+            db.session.commit()
+            return redirect('/home')
+        except:
+            return "Получилось ошибка"
+    else:
+        return render_template('create.html')
 
 
 @app.route('/genres')
 def genres():
-    return render_template('genres.html')
+    books = Book.query.all()
+    return render_template('genres.html', data=books)
 
 
 if __name__ == '__main__':
